2b616d94f23e62458574f6d0229af98daf91bd80
Wan <yuxi.wan@gmail.com>
2018-02-01 18:28:33 +0800

    Re-factoring the code

--- ad.js
+++ ad.js
@@ -1,65 +1,41 @@
-var d = document.createElement("iframe");
+/////////////////////////////////////
+//STEP 1: ADD AN EMPTY FRAME TO PARENT WINDOW
+/////////////////////////////////////
 
-d.frameBorder = 0;
-d.scrolling = "no";
-d.style.width = "300px";
-d.style.height = "200px";
-d.padding = 0;
-d.margin = 0;
+var frame = document.createElement("iframe");
 
-
-
-
+frame.frameBorder = 0;
+frame.scrolling = "no";
+frame.style.width = "416px";
+frame.style.height = "316px";
+frame.marginwidth = 0;
+frame.marginheight = 0;
+frame.hspace = 0;
+frame.vspace = 0;
 
 console.log(document.referrer);
-console.log(document.location);
+console.log(document.location.href);
 console.log(window.parent.document.title);
 
 var doc = window.parent.document;
+doc.documentElement.appendChild(frame);
 
-console.log(doc.documentElement);
+/////////////////////////////////////
+//STEP 2: INSERT HTML USING appendChild()
+/////////////////////////////////////
 
-
-doc.documentElement.appendChild(d);
-
-console.log(doc.documentElement.getElementsByTagName('iframe')[0]);
-var f = doc.documentElement.getElementsByTagName('iframe')[0];
-
-
-var fbody = f.contentWindow.document.getElementsByTagName("body")[0];
-
-
+var firstFrame = doc.documentElement.getElementsByTagName('iframe')[0];
+var fbody = firstFrame.contentWindow.document.getElementsByTagName("body")[0];
 console.log(fbody);
-
-//var newContent = document.createTextNode("Hi there and greetings!");
-
-
-htmlString = '<div style="width:300px; height:200px; background-color:#CCCCCC;"><img src="300x200.jpg" width="300" height="200"></div>'
 var newContent = document.createElement('div');
-newContent.innerHTML = htmlString.trim();
-
-
-function createElementFromHTML(htmlString) {
-    var div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
-
-    // Change this to div.childNodes to support multiple top-level nodes
-    return div.firstChild;
-}
-
-
-
+newContent.innerHTML = "<div style='height: 30%;  padding-top: 30%'> <h2 style='display: inline-block; width: 100%;text-align: center; vertical-align:middle'>How are you!</h2> </div>";
 fbody.appendChild(newContent);
 
+/////////////////////////////////////
+//STEP 3: INSERT HTML USING document.write (WILL OVERRIDE appendChild())
+/////////////////////////////////////
 
-
-
-//for (var p in doc) {
-//    var value = null;
-//    try {
-//        value = doc[p];
-//    } catch (err) {}
-//
-//    if (value)
-//        console.log(p, value);
-//}
+htmlString = '<img src="300x200.jpg" width="300" height="200">';
+htmlString += "<h2>Hi</h2>"
+jsString = ' <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id=\'cnzz_stat_icon_1272870994\'%3E%3C/span%3E%3Cscript src=\'" + cnzz_protocol + "s22.cnzz.com/stat.php%3Fid%3D1272870994%26show%3Dpic\' type=\'text/javascript\'%3E%3C/script%3E"));console.log(document);document.close();</script>';
+firstFrame.contentWindow.document.write(htmlString + jsString);
